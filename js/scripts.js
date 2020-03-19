9eec2efd3b6d1924fafc67f8933095a6cd674c6b
Your Nam <you@example.com>
2020-03-18 20:23:39 -0500

    Updated Project 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 var user = 'Visitor';
 var salutation = 'Welcome, ';
-var greeting = salutation + user + '! Here is our salad website.';
+var greeting = salutation + user + '! Here is our salad menu.';
 var greetingEl = document.getElementById('greeting');
 
 greetingEl.textContent = greeting;
+
+
+            var myIndex = 0;
+            autoslide();
+
+            function autoslide() {
+              var i;
+              var x = document.getElementsByClassName("slideshow");
+              for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";  
+              }
+              myIndex++;
+              if (myIndex > x.length) {myIndex = 1}    
+              x[myIndex-1].style.display = "block";  
+              setTimeout(autoslide, 2500);
+            }
